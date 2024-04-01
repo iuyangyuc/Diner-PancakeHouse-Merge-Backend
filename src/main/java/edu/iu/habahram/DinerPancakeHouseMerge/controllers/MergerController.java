6c153d5248546e6efbffbad9cff30ee995f3f934
Yang, Yuchen <yangyuc@iu.edu>
2024-04-01 08:34:01 -0400

    In class9-Part6

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,17 +1,13 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
-import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Menu;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -24,7 +20,7 @@ public class MergerController {
     }
 
     @GetMapping
-    public ArrayList<MenuItem> get() {
+    public ArrayList<Menu> get() {
         return mergerRepository.getTheMenus();
     }
 }
